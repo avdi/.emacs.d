c76993c9603f4a99002b4ae6375870b0999a086c
Avdi Grimm <avdi@avdi.org>
2015-05-23 18:23:25 -0400

    Switch to using auth-src for blogging credentials.

--- personal/blogging.el
+++ personal/blogging.el
@@ -3,13 +3,14 @@
 
 (require 'netrc)
 (require 'org2blog)
-(setq abg-netrc-vc (netrc-machine (netrc-parse "~/.netrc") "virtuouscode"))
+(require 'auth-source)
+(setq abg-devblog-credentials (auth-source-user-and-password "devblog.avdi.org"))
 (setq org2blog/wp-blog-alist
-      '(("virtuouscode"
-	 :url "http://devblog.avdi.org/xmlrpc.php"
-	 :username (netrc-get abg-netrc-vc "login")
-	 :password (netrc-get abg-netrc-vc "password")
-	 :tags-as-categories nil)))
+      `(("virtuouscode"
+             :url "http://devblog.avdi.org/xmlrpc.php"
+             :username ,(car abg-devblog-credentials)
+             :password ,(cadr abg-devblog-credentials)
+             :tags-as-categories nil)))
 
 ;; See https://github.com/punchagan/org2blog/issues/89#issuecomment-8013355
 (setq xml-entity-alist
