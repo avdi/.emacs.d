98b5abb5104feebf51e506c925cd0f9531b55440
Avdi Grimm <avdi@avdi.org>
2015-09-03 18:25:47 -0400

    Steal align config for ruby from defunkt

--- personal/ruby.el
+++ personal/ruby.el
@@ -2,13 +2,14 @@
 (prelude-require-package 'chruby)
 (prelude-require-package 'enh-ruby-mode)
 (prelude-require-package 'ruby-mode)
+(prelude-require-package 'ruby-additional)
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-to-list 'org-babel-load-languages
              '("ruby" . enh-ruby-mode))
 
-
+(add-to-list 'org-src-lang-modes '("ruby" . enh-ruby))
 (require 'cl)
 (require 'chruby)
 (chruby "2.1.3")
@@ -42,3 +43,32 @@
 (add-to-list 'org-babel-tangle-lang-exts '("ruby" . "rb"))
 
 (prelude-require-package 'rspec-mode)
+
+;; setup align for ruby-mode
+;; yoinked from https://github.com/defunkt/emacs/blob/master/vendor/ruby-hacks.el
+(require 'align)
+
+(defconst align-ruby-modes '(ruby-mode)
+  "align-perl-modes is a variable defined in `align.el'.")
+
+(defconst ruby-align-rules-list
+  '((ruby-comma-delimiter
+     (regexp . ",\\(\\s-*\\)[^/ \t\n]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-string-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\)['\"]\\w+['\"]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-symbol-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\):\\w+")
+     (modes  . align-ruby-modes)))
+  "Alignment rules specific to the ruby mode.
+See the variable `align-rules-list' for more details.")
+
+(add-to-list 'align-perl-modes 'ruby-mode)
+(add-to-list 'align-dq-string-modes 'ruby-mode)
+(add-to-list 'align-sq-string-modes 'ruby-mode)
+(add-to-list 'align-open-comment-modes 'ruby-mode)
+(dolist (it ruby-align-rules-list)
+  (add-to-list 'align-rules-list it))
