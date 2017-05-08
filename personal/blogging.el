9520602027a71f25d59b318feed90cc169f6e996
Avdi Grimm <avdi@avdi.org>
2017-05-08 13:18:38 -0400

    Update blog settings to refer to virtuouscode.com

--- personal/blogging.el
+++ personal/blogging.el
@@ -4,13 +4,13 @@
 (require 'netrc)
 (require 'org2blog)
 (require 'auth-source)
-(setq abg-devblog-credentials (auth-source-user-and-password "devblog.avdi.org"))
+(setq abg-devblog-credentials (auth-source-user-and-password "www.virtuouscode.com"))
 (setq org2blog/wp-blog-alist
       `(("virtuouscode"
-             :url "http://devblog.avdi.org/xmlrpc.php"
-             :username ,(car abg-devblog-credentials)
-             :password ,(cadr abg-devblog-credentials)
-             :tags-as-categories nil)))
+         :url "http://www.virtuouscode.com/xmlrpc.php"
+         :username ,(car abg-devblog-credentials)
+         :password ,(cadr abg-devblog-credentials)
+         :tags-as-categories nil)))
 
 ;; See https://github.com/punchagan/org2blog/issues/89#issuecomment-8013355
 (setq xml-entity-alist
