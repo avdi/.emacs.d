49f5649079c55d219537d22c47f8d4fa7dbe8051
Avdi Grimm <vendor@avdi.org>
2018-01-08 12:49:00 -0500

    Recent changes from caroline

--- personal/ruby.el
+++ personal/ruby.el
@@ -11,7 +11,7 @@ ruby version, and the gem path"
       (shell-command-to-string
        (concat ruby-bin "/ruby -rubygems -e 'print [(defined?(RUBY_ENGINE) ? RUBY_ENGINE : %[ruby]), (RUBY_VERSION), (Gem.default_dir.inspect)].join(%[##])' 2>/dev/null")) "##")))
 
-(prelude-require-package 'enh-ruby-mode)
+(prelude-require-package 'ruby-mode)
 (prelude-require-package 'ruby-mode)
 (prelude-require-package 'ruby-additional)
 
@@ -20,7 +20,7 @@ ruby version, and the gem path"
 (require 'ob-ruby)
 (add-to-list 'org-babel-load-languages
              '(ruby . t))
-(add-to-list 'org-src-lang-modes '("ruby" . ruby))
+(add-to-list 'org-src-lang-modes '("enh-ruby" . ruby))
 
 (add-hook 'enh-ruby-mode-hook 'turn-off-smartparens-mode)
 (add-hook 'ruby-mode-hook 'turn-off-smartparens-mode)
